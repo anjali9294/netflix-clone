016e317753b66c500ed8ae6315af72a6fd8565f0
anjali9294 <anjaliparihar9294@gmail.com>
2023-02-15 11:41:20 +0530

    login and register route done with incrypted password

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+//   routes import
+const authRoute = require("./routes/auth");
 const app = express();
 dotenv.config();
 // connecting database
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.listen(3000, () => {
+app.use(express.json());
+//rouets
+app.use("/api/auth", authRoute);
+
+app.listen(4000, () => {
   console.log("server is running");
 });
