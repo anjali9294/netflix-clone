8a8f0677ef996e615d41c6bb60cb5957b3d00104
anjali9294 <anjaliparihar9294@gmail.com>
2023-02-15 11:59:28 +0530

    jwt token set and verify method is aslo done

--- routes/auth.js
+++ routes/auth.js
@@ -33,13 +33,13 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password or username!");
 
-    //   const accessToken = jwt.sign(
-    //     { id: user._id, isAdmin: user.isAdmin },
-    //     process.env.SECRET_KEY,
-    //     { expiresIn: "5d" }
-    //   );
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
     const { password, ...info } = user._doc;
-    res.status(200).json(info);
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
